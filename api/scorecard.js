855472b5e1de14d942d749f7a2a56e8570ac417f
onlyzahid018-tech <onlyzahid018@gmail.com>
2026-05-30 16:43:39 +0530

    Create scorecard API handler
    Implement API handler to fetch cricket scorecard data.

---
+++ api/scorecard.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/40381/hscard",
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "cricbuzz-cricket.p.rapidapi.com",
+          "x-rapidapi-key": "3369e19d10msh91a7df734747469p12868ejsn9c331e41f555"
+        }
+      }
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+
+  } catch (error) {
+    res.status(500).json({
+      error: "Failed to fetch scorecard",
+      details: error.message
+    });
+  }
+}
